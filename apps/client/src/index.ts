75fa548acad6f32187eb6f1bf9e8c8fec11e7ce2
Thomas <thomascode92@hotmail.com>
2025-08-14 07:56:02 +0200

    Starting the MCP Server

--- apps/client/src/index.ts
+++ apps/client/src/index.ts
@@ -1,1 +1,14 @@
-(async function main() {})();
+import { spawn } from "node:child_process";
+import * as readline from "node:readline/promises";
+
+(async function main() {
+  const serverProcess = spawn("tsx", ["../server/src/index.ts"], {
+    stdio: ["pipe", "pipe", "inherit"],
+    cwd: "../server",
+  });
+
+  const rl = readline.createInterface({
+    input: serverProcess.stdout,
+    output: undefined,
+  });
+})();
